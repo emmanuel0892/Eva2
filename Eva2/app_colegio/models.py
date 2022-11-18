7591c909c71223b0f30f80bd4e3f8acd1967d841
Emmanuel Rubio <emmanuel.rubio.gaete92@gmail.com>
2022-11-18 19:47:30 -0300

    modulo registrar listo

--- Eva2/app_colegio/models.py
+++ Eva2/app_colegio/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class alumnos (models.Model):
+    rut = models.TextField(max_length=20, null=False)
     nombre = models.TextField(max_length=50, null=False)
-    apePaterno = models.TextField(max_length=50, null=False)
-    apeMaterno = models.TextField(max_length=50, null=False)
+    apellidos = models.TextField(max_length=50, null=False)
     sexo = models.TextField(max_length=50, null=False)
     curso = models.TextField(max_length=50, null=False)
     edad = models.IntegerField(validators=[MinValueValidator(5), MaxValueValidator(30)] ,null=False)
     ciudad = models.TextField(max_length=50, null=False)
-    comnuna = models.TextField(max_length=50, null=False)
+    comuna = models.TextField(max_length=50, null=False)
--- Eva2/app_colegio/views.py
+++ Eva2/app_colegio/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from app_colegio.models import alumnos
 
 def inicio(request):
     return render (request, 'index.html')
 
 def registrar(request):
+
+    if request.method == 'POST':
+
+        rut = request.POST['txtRut']
+        nom = request.POST['txtNom']
+        ape = request.POST['txtApe']
+        sex = request.POST['opSex']
+        cur = request.POST['cboCurso']
+        eda = request.POST['txteda']
+        ciu = request.POST['cboCiu']
+        com = request.POST['txtComu']
+
+        comprobarRut = alumnos.objects.filter(rut=rut)
+
+        if comprobarRut:
+            datos = { 'r2' : 'El rut ('+rut+') ya se encuentra registrado!!!' }
+            return render(request, 'form_registrar.html', datos)
+        else:
+            alum = alumnos(rut=rut, nombre=nom, apellidos=ape, sexo=sex, curso=cur, edad=eda, ciudad=ciu, comuna=com)
+            alum.save()
+            datos = { 'r' : 'El alumno se registró correctamente!!!' }
+            return render(request, 'form_registrar.html', datos)
     return render(request, 'form_registrar.html')
 
 def listado(request):
